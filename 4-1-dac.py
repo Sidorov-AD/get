27b9523644025682711eba218fde03a3ac6f9658
Паршуто <vladdj2@mail.ru>
2025-04-16 12:55:08 +0300

    Add 4-1-dac.py

---
+++ 4-1-dac.py
@@ -0,0 +1,42 @@
+import RPi.GPIO as GPIO
+
+dac = [8, 11, 7, 1, 0, 5, 12, 6]
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(dac, GPIO.OUT)
+
+def decimal2binary(value):
+    return [int(element) for element in bin(value)[2:].zfill(8)]
+
+def GetVoltage(value):
+    d = 3.3 * 1/2
+    voltage = 0
+    for i in value:
+        voltage += i * d
+        d /= 2
+    return voltage
+
+
+try:
+    while True:
+        val = input("Write num from 0 to 255: ")
+
+        try:
+            val = int(val)
+            if 0 <= val <= 255:
+                val_bin = decimal2binary(val)
+                GPIO.output(dac, val_bin)
+                print(f"Voltage is about {GetVoltage(val_bin):.4} volt")
+            else:
+                print("Number must be in range [0,255]!")
+
+        except Exception:
+            if val == "q": 
+                break
+            else:
+                print("You have to type a number, not string! Try again...")
+
+
+finally:
+    GPIO.output(dac, 0)
+    GPIO.cleanup()
